e36d76b5ee6e016ef19aa3cdbbf2a04343c38d83
amphonie <114940666+amphonie@users.noreply.github.com>
2023-01-17 08:02:55 -0500

    Pong! (WIP)
    Still Work in Progress.

--- Exercise 1/README.md
+++ Exercise 1/README.md
@@ -1,19 +1,17 @@
-# Template p5 project
+# Pong! (WIP)
 
-## What is a read me file?
-This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
+### Controls
 
-README files are written in [markdown](https://www.markdownguide.org/cheat-sheet/) to format them. Your readme file should contain all the information about your project, and if needed, how to run it. If you need or use libraries or dependencies, list them here and link to where the user can find the appropriate information. 
+- SPACE: Start Game
+- W & S: Player 1 Move
+- UP & DOWN: Player 2 Move
 
-## This template
-This is a p5js template for CART263. You can use this as a starting point for your project. To use this template: 
-* Copy the folder to your computer 
-* Open it in your favorite code editor, we suggest VSCode
-* Install the live server extension 
-* Click "Go Live" and view your code!
+### Exercise details
 
-## Editing files
-* **Index.html** contains your HTML structure of the project. If you need to add libraries or other files of code, link them in this file. 
-* **js/script.js** this is where you edit most of your P5 code.  
-* **js/libraries** this is where you add any additional libraries.
-* **assets** folder contains any images, videos, audio or other files needed to make your project work. 
+Create a Pong game where 2 players bounce a bll back and forth horizontally to score points.
+
+- 2 paddles that move vertically using Up & Down arrows, and W & S.
+- A ball should bounce between the two paddles
+- A point is scored when the ball passes the opponents's paddle without being hit.
+- The ball should hit the walls of the screen and bounce on an angle.
+- The first player to 10 points win.
--- Exercise 1/js/script.js
+++ Exercise 1/js/script.js
@@ -1,12 +1,23 @@
 /**
-Title of Project
-Author Name
+Pong (WIP)
+Anthony Bourgeois
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This is an attempt to make pong with as far as my current own knowledge of P5.JS can take me.
+I haven't seen object oriented programing yet and prefer to explore it once saw in class! 
 */
 
 "use strict";
+var size = 20 // size of the paddle
+var playerOnePosition = 220 // Initial left paddle position
+var playerTwoPosition = 220 // Initial right paddle position
+var playerSpeed = 5 // Paddle speed
+
+var puckSize = 10 // size of the puck
+var puckX = 350;
+var puckY = 250;
+var puckDirX;
+var puckDirY;
+var puckSpeed = 0.01;
 
 
 /**
@@ -21,8 +32,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-    createCanvas(500,500);
-    background(0,255,0);
+    createCanvas(700,500);
 }
 
 
@@ -30,5 +40,65 @@ function setup() {
 Description of draw()
 */
 function draw() {
+background(0,0,0);
+
+for (let i = 0; i < width/size; i++) { // Drawing the middle line 
+  fill("#696969")
+  rect(350,(i*size), 8,size);
+  i++
+}
+
+ player1();
+ player2();
+ puck();
+}
+
+function player1(){ //Left player paddle
+      if (keyIsDown(87)) { // if "W" Key is pressed down
+        playerOnePosition -= playerSpeed;} // decrease speed to a negative value
+          if (playerOnePosition < 0) { // if the position is lower than 0
+            playerOnePosition = 0; // set the position to 0
+          }
+          if (playerOnePosition >= 440) { //if the paddle position is higher than 440 ( size of the canvas - the size of the paddle)
+            playerOnePosition = 440; //set the position to 440
+          }
+     
+      if (keyIsDown(83)) { //if the "s" key is held down 
+        playerOnePosition += playerSpeed;} //increment speed to negative value 
+
+      fill(255,255,255); //Color the paddle white
+      rect(10,playerOnePosition,10,60); //paddle size
+    }
+
+function player2(){ //Right player paddle
+    if (keyIsDown(UP_ARROW)) { // if the Up Arrow Key is pressed down
+      playerTwoPosition -= playerSpeed; // decrease speed to a negative value
+    }   if (playerTwoPosition < 0) { // if the position is lower than 0 
+          playerTwoPosition = 0; // set the position to 0
+    }
+    if (playerTwoPosition >= 440) { //if the paddle position is higher than 440 ( size of the canvas - the size of the paddle)
+      playerTwoPosition = 440; //set the position to 440
+    } 
+  
+    if (keyIsDown(DOWN_ARROW)) { //if the Down Arrow Key is held down 
+        playerTwoPosition += playerSpeed; //increment speed to negative value 
+    }
+  fill(255,255,255); //Make paddle white
+  rect(680,playerTwoPosition,10,60); //Right Paddle Rect.
+}
+
+function puck(){ //Puck 
+  noStroke();
+  fill(255,0,0);
+  square(puckX, puckY, puckSize); 
+  
+  // if (false) {
+  //   puckX = puckX + puckSpeed;
+  //   puckSpeed = puckSpeed + 0.2;
+  // }
+
+  // puckDirX = puckX * puckSpeed
+  // puckDirY = puckY * puckSpeed
 
 }
+
